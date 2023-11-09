a4f24588f6143cf15e381bbf20e823295b05cb4d
Muhammad Raihan Satrio <adewipo@gmail.com>
2023-11-09 15:35:53 +0700

    chore: add user entity

---
+++ src/modules/user/entities/user.entity.ts
@@ -0,0 +1,16 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn("uuid")
+  public id: string;
+
+  @Column()
+  public name: string;
+
+  @Column()
+  public email: string;
+
+  @Column()
+  public password: string;
+}
