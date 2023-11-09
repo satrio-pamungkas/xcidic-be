dae46224e84d5fcd167e16e112068eb57d65c318
Muhammad Raihan Satrio <adewipo@gmail.com>
2023-11-09 15:49:16 +0700

    feat: add migration for user entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,24 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './modules/user/user.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import typeorm from './shared/configs/typeorm.config';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [typeorm]
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => (
+        configService.get("typeorm")
+      )
+    }),
+    UserModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
